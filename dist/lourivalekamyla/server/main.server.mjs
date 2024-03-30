08079191ebe145f04ef4185659d93eae2ff76ec9
Hugo Paul <hugo.paul@hotmail.com>
2024-03-30 19:42:16 -0300

    added dist

--- dist/lourivalekamyla/server/main.server.mjs
+++ dist/lourivalekamyla/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VHJADYND.mjs";import"./chunk-TPQFQJPV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-W5WFIZA5.mjs";import"./chunk-TPQFQJPV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
