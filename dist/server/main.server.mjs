3eea2df6762e07857d143183c0b34f42ce718a30
Hugo Paul <hugo.paul@hotmail.com>
2024-08-22 23:17:54 -0300

    domain start

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-URFLYXK7.mjs";import"./chunk-QZKHOOSX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-U7BCR6LQ.mjs";import"./chunk-QZKHOOSX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
