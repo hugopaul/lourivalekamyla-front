63be3572c6db826163a31b7188229476a464e5d0
Hugo Paul <hugo.paul@hotmail.com>
2024-08-31 15:19:07 -0300

    mudando path

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ARI5NPWB.mjs";import"./chunk-QZKHOOSX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TLJ5FORH.mjs";import"./chunk-QZKHOOSX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
